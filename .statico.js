efa0a0451d2b33635108d791fb445577a1b1b5b8
GordonAnsell <moving@ga1964.com>
2022-03-25 07:58:49 +0000

    Update .statico.js

--- .statico.js
+++ .statico.js
@@ -26,13 +26,13 @@ async function afterParsedTemplateFile(cfg, tf)
     // Save the URL.
     let url = cfg.hostname + tf.data.permalink;
 
-    if (tf.data.permalink.indexOf("Star Trek: Discovery") !== 0) {
-        syslog.warning("In here");
+    if (tf.data.permalink.indexOf("Star Trek: Discovery") !== -1) {
+        syslog.warning("In here:");
     }
 
     // Received.
     let wmentions = proc.mentionsForUrl(url);
-    if (tf.data.permalink.indexOf("Star Trek: Discovery") !== 0) {
+    if (tf.data.permalink.indexOf("Star Trek: Discovery") !== -1) {
         syslog.inspect(wmentions, "error");
     }
     if (wmentions && wmentions.length > 0) {
